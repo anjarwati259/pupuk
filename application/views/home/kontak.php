9bcef25fcd2fc16c207240fdde97d828f6e43486
anjarwati259 <34015961+anjarwati259@users.noreply.github.com>
2021-06-24 16:30:33 +0700

    membuat kontak

--- application/views/home/kontak.php
+++ application/views/home/kontak.php
@@ -1,90 +1,74 @@
-<section class="ftco-section">
+<style type="text/css">
+	.kontak{
+		width: 100%;
+		height: auto;
+		background-color: #F5F5F5;
+	}
+	.kontak-inner{
+		width: 100%;
+		height: auto;
+		background-color: #fff;
+		padding: 24px;
+	}
+	.map iframe{
+		width: 100%;
+		height: 340px;
+		border: none;
+	}
+	.contact-text{
+		width: 100%;
+		height: auto;
+	}
+	.contact-text h1{
+		font-size: 36px;
+		font-weight: 600;
+		color: #50AB34;
+		margin: 0;
+		padding-bottom: 20px;
+	}
+	.contact-text h2{
+		font-size: 20px;
+		font-weight: 500;
+		color: #444;
+		margin: 0;
+		padding-top: 25px;
+	}
+	.contact-text h2 i{
+		color: #50AB34;
+	}
+	.contact-text p{
+		font-size: 14px;
+		color: #000;
+		margin: 0;
+		padding-top: 5p;
+		padding-left: 24px;
+		font-weight: 100;
+	}
+</style>
+<div class="kontak">
 	<div class="container">
-		<div class="row justify-content-center">
-			<div class="col-md-12">
-				<div class="wrapper">
-					<div class="row no-gutters">
-						<div class="col-lg-8 col-md-7 order-md-last d-flex align-items-stretch">
-							<div class="contact-wrap w-100 p-md-5 p-4">
-								<h3 class="mb-4">Get in touch</h3>
-								<div id="form-message-warning" class="mb-4"></div> 
-								<form method="POST" id="contactForm" name="contactForm" class="contactForm">
-									<div class="row">
-										<div class="col-md-6">
-											<div class="form-group">
-												<label class="label" for="name">Full Name</label>
-												<input type="text" class="form-control" name="name" id="name" placeholder="Name">
-											</div>
-										</div>
-										<div class="col-md-6"> 
-											<div class="form-group">
-												<label class="label" for="email">Email Address</label>
-												<input type="email" class="form-control" name="email" id="email" placeholder="Email">
-											</div>
-										</div>
-										<div class="col-md-12">
-											<div class="form-group">
-												<label class="label" for="subject">Subject</label>
-												<input type="text" class="form-control" name="subject" id="subject" placeholder="Subject">
-											</div>
-										</div>
-										<div class="col-md-12">
-											<div class="form-group">
-												<label class="label" for="#">Message</label>
-												<textarea name="message" class="form-control" id="message" cols="30" rows="4" placeholder="Message"></textarea>
-											</div>
-										</div>
-										<div class="col-md-12">
-											<div class="form-group">
-												<input type="submit" value="Send Message" class="btn btn-primary">
-												<div class="submitting"></div>
-											</div>
-										</div>
-									</div>
-								</form>
-							</div>
-						</div>
-						<div class="col-lg-4 col-md-5 d-flex align-items-stretch">
-							<div class="info-wrap bg-primary w-100 p-md-5 p-4">
-								<h3>Let's get in touch</h3>
-								<p class="mb-4">We're open for any suggestion or just to have a chat</p>
-			        	<div class="dbox w-100 d-flex align-items-start">
-			        		<div class="icon d-flex align-items-center justify-content-center">
-			        			<span class="fa fa-map-marker"></span>
-			        		</div>
-			        		<div class="text pl-3">
-				            <p><span>Address:</span> 198 West 21th Street, Suite 721 New York NY 10016</p>
-				          </div>
-			          </div>
-			        	<div class="dbox w-100 d-flex align-items-center">
-			        		<div class="icon d-flex align-items-center justify-content-center">
-			        			<span class="fa fa-phone"></span>
-			        		</div>
-			        		<div class="text pl-3">
-				            <p><span>Phone:</span> <a href="tel://1234567920">+ 1235 2355 98</a></p>
-				          </div>
-			          </div>
-			        	<div class="dbox w-100 d-flex align-items-center">
-			        		<div class="icon d-flex align-items-center justify-content-center">
-			        			<span class="fa fa-paper-plane"></span>
-			        		</div>
-			        		<div class="text pl-3">
-				            <p><span>Email:</span> <a href="mailto:info@yoursite.com">info@yoursite.com</a></p>
-				          </div>
-			          </div>
-			        	<div class="dbox w-100 d-flex align-items-center">
-			        		<div class="icon d-flex align-items-center justify-content-center">
-			        			<span class="fa fa-globe"></span>
-			        		</div>
-			        		<div class="text pl-3">
-				            <p><span>Website</span> <a href="#">yoursite.com</a></p>
-				          </div>
-			          </div>
-		          </div>
-						</div>
+		<div class="kontak-inner">
+			<div class="row">
+				<div class="col-md-8">
+					<div class="map">
+						<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4696.703027018027!2d113.70169207756429!3d-8.158271665562678!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd69441c126be99%3A0xa910a197f321ce2d!2sKantor%20Pusat%20Pemasaran%20PUPUK%20KILAT!5e0!3m2!1sid!2sid!4v1624519646279!5m2!1sid!2sid" width="100%" height="auto" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="contact-text">
+						<h1>Contact Us</h1>
+						<h2><i class="fa fa-map-marker" aria-hidden="true"></i>&nbsp; Address</h2>
+						<p>Jl. Manggis Raya No.11 Jemberlor Patrang, Jember, Jawa Timur
+						</p>
+						<h2><i class="fa fa-phone" aria-hidden="true"></i>&nbsp; Phone</h2>
+						<p>+6281 335 005 334
+						</p>
+						<h2><i class="fa fa-envelope" aria-hidden="true"></i>&nbsp; Email</h2>
+						<p>ptagiofficial@gmail.com
+						</p>
 					</div>
 				</div>
 			</div>
 		</div>
 	</div>
-</section>
+</div>
--- application/views/layout/nav.php
+++ application/views/layout/nav.php
@@ -1,21 +1,21 @@
-		<!-- NAVIGATION -->
-		<nav id="navigation">
-			<!-- container -->
-			<div class="container">
-				<!-- responsive-nav -->
-				<div id="responsive-nav">
-					<!-- NAV -->
-					<ul class="main-nav nav navbar-nav">
-						<li><a href="<?php echo base_url() ?>">Home</a></li>
-						<li><a href="<?php echo base_url('home/page_order') ?>">Order</a></li>
-						<li><a href="<?php echo base_url('home/produk') ?>">Produk</a></li>
-						<li><a href="https://ptagi.co.id" target="_blank">Tentang Kami</a></li>
-						<li><a href="#">Kontak</a></li>
-					</ul>
-					<!-- /NAV -->
-				</div>
-				<!-- /responsive-nav -->
-			</div>
-			<!-- /container -->
-		</nav>
-		<!-- /NAVIGATION -->
+<!-- NAVIGATION -->
+<nav id="navigation">
+	<!-- container -->
+	<div class="container">
+		<!-- responsive-nav -->
+		<div id="responsive-nav">
+			<!-- NAV -->
+			<ul class="main-nav nav navbar-nav">
+				<li><a href="<?php echo base_url() ?>">Home</a></li>
+				<li><a href="<?php echo base_url('home/page_order') ?>">Order</a></li>
+				<li><a href="<?php echo base_url('home/produk') ?>">Produk</a></li>
+				<li><a href="https://ptagi.co.id" target="_blank">Tentang Kami</a></li>
+				<li><a href="<?php echo base_url('home/kontak') ?>">Kontak</a></li>
+			</ul>
+			<!-- /NAV -->
+		</div>
+		<!-- /responsive-nav -->
+	</div>
+	<!-- /container -->
+</nav>
+<!-- /NAVIGATION -->
